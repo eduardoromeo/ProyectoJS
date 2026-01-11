23b08d36b44e101a2f24134925ddb51aad1a3b11
eduardo moreira T. <38986601+eduardoromeo@users.noreply.github.com>
2026-01-11 00:50:25 -0400

    new push firs commit

---
+++ js/pages/about.js
@@ -0,0 +1,6 @@
+export function renderAboutPage(root){
+    root.innerHTML = `
+        <h1>Acerca de </h1>
+        <p>Esta es la página acerca de</p>
+    `
+}
---
+++ js/pages/home.js
@@ -0,0 +1,6 @@
+export function renderHomePage(root){
+    root.innerHTML = `
+        <h1>Home </h1>
+        <p>Esta es la página home</p>
+    `
+}
