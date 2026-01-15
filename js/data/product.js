de841ccdae9111ed956da1fc1f66185129f01643
eduardo moreira T. <38986601+eduardoromeo@users.noreply.github.com>
2026-01-15 02:47:59 -0400

    header completed

--- js/data/product.js
+++ js/data/product.js
@@ -1,6 +1,6 @@
 export const productsSeed = [
-    { id: "p-001", name :"Teclado Mecánico", category: "Periféricos", price: 450, stock:12, sold:34, active:true},
-    { id: "p-002", name :"Mouse Gamer", category: "Periféricos", price: 180, stock:25, sold:51, active:true},
-    { id: "p-003", name :"Monitor 24' '", category: "Pantallas", price: 1200, stock:6, sold:18, active:true},
-    { id: "p-004", name :"SSD 1TB", category: "Almacenamiento", price: 650, stock:9, sold:40, active:true},
+    { id: "p-001", name :"Matrimonios", category: "Catálogo", price: 450, stock:12, sold:34, active:true},
+    { id: "p-002", name :"Bautizos", category: "Catálogo", price: 180, stock:25, sold:51, active:true},
+    { id: "p-003", name :"Promociones", category: "Catálogo", price: 1200, stock:6, sold:18, active:true},
+    { id: "p-004", name :"Cumpleaños", category: "Catálogo", price: 650, stock:9, sold:40, active:true},
 ]
